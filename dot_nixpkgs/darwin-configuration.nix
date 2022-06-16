7f43e2e1b8f16fd834a254b9cc12c99b4a4b44ea
@lrcf <89586611+lrcf@users.noreply.github.com>
2022-06-16 19:09:36 +0200

    Add opinionated system defaults

--- dot_nixpkgs/darwin-configuration.nix
+++ dot_nixpkgs/darwin-configuration.nix
@@ -4,6 +4,7 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+  time.timeZone = "Europe/Brussels";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
@@ -17,8 +18,75 @@
       pkgs.vim
     ];
 
-  # Create /etc/bashrc that loads the nix-darwin environment.
+  # Configure default editor and language variable for shell apps.
+  environment.variables = {
+    EDITOR = "vim";
+    LANG = "en_GB.UTF-8";
+  };
+
+  # Load nix-darwin environment in bash, fish, and zsh.
   programs.bash.enable = true;
-  programs.fish.enable = true;
-  programs.zsh.enable = true;
+  programs.fish.enable = true; # Must be post-installed
+  programs.zsh.enable = true; # Default in Catalina+
+
+  system.defaults = {
+    finder = {
+      AppleShowAllExtensions = true;
+      QuitMenuItem = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+    };
+
+    universalaccess = {
+      reduceTransparency = false;
+    };
+
+    NSGlobalDomain = {
+      AppleShowScrollBars = "Always";
+
+      # System keyboard settings
+      KeyRepeat = 2;
+      InitialKeyRepeat = 15;
+
+      # System locale settings
+      AppleMeasurementUnits = "Centimeters";
+      AppleMetricUnits = 1;
+
+      # Expand print panel by default
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+
+      # Expand save panel by default
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+
+      # Save to iCloud by default
+      NSDocumentSaveNewDocumentsToCloud = true;
+
+      # Show control characters in caret notation
+      NSTextShowsControlCharacters = true;
+
+      # NSQuitAlwaysKeepsWindows
+
+      NSAutomaticSpellingCorrectionEnabled = false;
+    };
+  };
+
+  system.activationScripts.postActivation.text =
+    ''
+    # PR pending for a year: LnL7/nix-darwin#230 defaults write -g AppleHighlightColor 0.968627 0.831373 1.000000 Purple
+
+    # System locale settings
+    defaults write -g AppleLanguages -array "en-GB" "cs-CZ" "sk-SK"
+    defaults write -g AppleLocale -string "en_CZ"
+
+    # Disable resume after restart by default
+    defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+
+    # Enable Help Viewer Dev Mode and set their windows to not float
+    defaults write com.apple.helpviewer DevMode -bool true
+
+    # Save screen captures in .png if they’re saved as files.
+    defaults write com.apple.screencapture type -string "png"
+    '';
 }
