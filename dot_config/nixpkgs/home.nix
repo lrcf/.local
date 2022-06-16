126067df99da56711d56b91d8d0d92ebbccdb486
@lrcf <89586611+lrcf@users.noreply.github.com>
2022-06-16 19:25:16 +0200

    Fix environment variables

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -2,12 +2,12 @@
 
 {
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  # This value determines the Home Manager release that your
+  #
+  # `stateVersion` determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
   # incompatible changes.
+  programs.home-manager.enable = true;
   home.stateVersion = "22.05";
 
   # Home Manager needs a bit of information about you and the
@@ -15,10 +15,17 @@
   home.username = "lrcf";
   home.homeDirectory = "/Users/lrcf";
 
-  home.packages =
-    [ pkgs.chezmoi
-      pkgs.gnupg1
-      pkgs.nodejs-18_x
-      pkgs.php
-    ];
+  # User installed packages
+  home.packages = [
+    pkgs.chezmoi
+    pkgs.gnupg1
+    pkgs.nodejs-18_x
+    pkgs.php
+  ];
+
+  # Set environment variables
+  home.sessionVariables = {
+    EDITOR = "vim";
+    LANG = "en_GB.UTF-8";
+  };
 }
